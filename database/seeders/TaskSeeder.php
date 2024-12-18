d4414017ac312bff5d309fc0afce529d08cb017b
chunnt24 <chung2k4@gmail.com>
2024-12-18 13:45:09 +0700

    done

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -39,4 +39,5 @@ class TaskSeeder extends Seeder
         }
     }
 
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 Route::resource('tasks', TaskController::class);
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
 });
